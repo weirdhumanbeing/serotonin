11f22a9509c2abda95b3644e82fd3e4cef0c86ec
weirdhumanbeing <46325811+weirdhumanbeing@users.noreply.github.com>
2022-05-13 19:47:15 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CountDownToSerotonin.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="StyleSheet1.css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server" />
+            <asp:UpdatePanel runat="server">
+                <ContentTemplate>
+                    <asp:Label ID="Text1" Class="Text" Text="Iki pilnos laimės liko:" runat="server" />
+                    <asp:Timer ID="timer" runat="server" Interval="1000">
+                    </asp:Timer>
+                    <br />
+                    <br />
+                    <asp:Label ID="Time1" Class="Text" runat="server" Text="Label"></asp:Label>
+                    <br />
+                    <br />
+                    <asp:Label ID="Label2" Class="Text" Text="Arba" runat="server" />
+                    <br />
+                    <br />
+                    <asp:Label ID="Time2" Class="Text" runat="server" Text="Label"></asp:Label>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountDownToSerotonin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime dt = new DateTime(2022, 06, 24);
+            //DateTime test = new DateTime(2022, 05, 9);
+            TimeSpan dt2 = dt - DateTime.Now;
+            int weeksResidue = dt2.Days % 7;
+            int days = dt2.Days - weeksResidue;
+            int weeks = (dt2.Days - weeksResidue) / 7;
+            Time2.Text = weeks.ToString() + " savaitės";
+            Time1.Text = dt2.Days.ToString() + " dienos, " + dt2.Hours.ToString() + " valandos, " + dt2.Minutes.ToString() + " minutės, " + dt2.Seconds.ToString() + " sekundės";
+
+        }
+
+        /*public string CalculateTimeDifference(DateTime startDate, DateTime endDate)
+        {
+            int days = 0; int hours = 0; int mins = 0; int secs = 0;
+            string final = string.Empty;
+            if (endDate > startDate)
+            {
+                days = (endDate - startDate).Days;
+                hours = (endDate - startDate).Hours;
+                mins = (endDate - startDate).Minutes;
+                secs = (endDate - startDate).Seconds;
+                final = string.Format("{0} days {1} hours {2} mins {3} secs", days, hours, mins, secs);
+            }
+            return final;
+        }*/
+
+    }
+
+}
